aa19125f298280305e072ef1ff87b6d223acef9f
a8ksh4 <spam.dn@gmail.com>
2015-10-11 00:03:29 -0700

    working on peg

--- Python/peg_solitare.py
+++ Python/peg_solitare.py
@@ -1,19 +1,20 @@
 #!/usr/bin/env python
+#!/usr/bin/env pypy
 import itertools as it
+import sys
+from copy import deepcopy
 
-board = [[None, None, None, True, True, True, None, None, None],
-         [None, None, None, True, True, True, None, None, None],
-         [None, None, None, True, True, True, None, None, None],
-         [True, True, True, True, True, True, True, True, True],
-         [True, True, True, True, False, True, True, True, True],
-         [True, True, True, True, True, True, True, True, True],
-         [None, None, None, True, True, True, None, None, None],
-         [None, None, None, True, True, True, None, None, None],
-         [None, None, None, True, True, True, None, None, None]]
+BOARD = [[None, None, True, True, True, None, None],
+         [None, None, True, True, True, None, None],
+         [True, True, True, True, True, True, True],
+         [True, True, True, False, True, True, True],
+         [True, True, True, True, True, True, True],
+         [None, None, True, True, True, None, None],
+         [None, None, True, True, True, None, None]]
 
 def listMoves(board):
     moves = []
-    for x,y in it.permutations(range(9), 2):
+    for x,y in it.permutations(range(7), 2):
         if not board[x][y]:
             continue
         for xod, yod, xmd, ymd in ( (-2,  0, -1,  0),
@@ -22,7 +23,7 @@ def listMoves(board):
                                 ( 0,  2,  0,  1) ):
             move = (x, y, x+xod, y+yod, x+xmd, y+ymd)
             xo, yo, xm, ym = move[2:]
-            if xo >= 9 or xo < 0 or yo >= 9 or yo < 0:
+            if xo >= 7 or xo < 0 or yo >= 7 or yo < 0:
                 continue
             if None in (board[xo][yo], board[xm][ym]):
                 continue
@@ -31,31 +32,56 @@ def listMoves(board):
     return moves
 
 def applyMove(board, move):
+    board = deepcopy(board)
     x, y, xo, yo, xm, ym = move
     board[x][y] = False
     board[xm][ym] = False
     board[xo][yo] = True
     return board
 
-def genNext(field, level=0):
-    print field['board']
-    if level == DONE:
-        BREAK = True
+def countBoard(board):
+    sum = 0
+    for row in board:
+        #print row
+        for col in row:
+            if col == True:
+                sum += 1
+    return sum
 
-    rawread('next')
+def genNext(board, level=0):
+    numLeft = countBoard(board)
+    #print level, numLeft, " - ", level + numLeft
+    if numLeft <= LIMIT:
+        print level, numLeft, " - ", level + numLeft
+        print MOVES
+        #for row in board:
+        #    print row
+        print "Level was:", level
+        #testMoves()
+        sys.exit()
+    for move in listMoves(board):
+        MOVES.append(move)
+        genNext(applyMove(board, move), level+1)
+        MOVES.pop()
 
-    for move in listMoves(field['board']):
-        if BREAK:
-            break
-        new = {'board': applMove(field['board']), 'sub':{} }
-        field['sub'][move] = genNext(new, level+1)
-    return field
+def printBoard(board):
+    for row in board:
+        print row
 
-base_move = (2,4,4,4,3,4)
-field = {'board': applMove(board, base_move), 'sub':{} }
-BREAK = False
+def testMoves():
+    board = BOARD
+    printBoard(board)
+    for move in MOVES:
+        raw_input()
+        print move
+        board = applyMove(board, move)
+        printBoard(board)
+
+
+MOVES = [(2,4,4,4,3,4),]
 LEVELS = 0
-DONE = 41
+LIMIT = 3
     
 if __name__ == '__main__':
-    print listMoves(board)
+    #print countBoard(BOARD, 1)
+    genNext(applyMove(BOARD, MOVES[0]), 1)
---
+++ Python/peg_solitare3.py
@@ -0,0 +1,137 @@
+#!/usr/bin/env pypy
+import itertools as it
+import sys
+from copy import deepcopy
+
+BASE_BOARD = \
+        [[None, None, True, True, True, None, None],
+         [None, None, True, True, True, None, None],
+         [True, True, True, True, True, True, True],
+         [True, True, True, True, True, True, True],
+         [True, True, True, True, True, True, True],
+         [None, None, True, True, True, None, None],
+         [None, None, True, True, True, None, None]]
+
+LAST = (3, 3)
+BASE_BOARD[LAST[0]][LAST[1]] = False
+BOARDS = []
+for x in range(33):
+    BOARDS.append(deepcopy(BASE_BOARD))
+
+MOVES = [(1,3,3,3,2,3),]
+LEVEL = 0
+LIMIT = 9
+
+SPOTS = []
+for x in range(7):
+    for y in range(7):
+        SPOTS.append((x, y))
+#SPOTS = tuple(it.permutations(range(7), 2))
+def listMoves():
+    moves = []
+    for x,y in SPOTS:
+        if not BOARDS[LEVEL][x][y]:
+            continue
+        for xod, yod, xmd, ymd in ( (-2,  0, -1,  0),
+                                ( 2,  0,  1,  0),
+                                ( 0, -2,  0, -1),
+                                ( 0,  2,  0,  1) ):
+            move = (x, y, x+xod, y+yod, x+xmd, y+ymd)
+            xo, yo, xm, ym = move[2:]
+            if xo >= 7 or xo < 0 or yo >= 7 or yo < 0:
+                continue
+            if None in (BOARDS[LEVEL][xo][yo], BOARDS[LEVEL][xm][ym]):
+                continue
+            if BOARDS[LEVEL][xo][yo] == False and BOARDS[LEVEL][xm][ym] == True:
+                moves.append(move)
+    return moves
+
+def applyMove(move):
+    global BOARDS
+    #if LEVEL==1:
+    #    printBoard(2, ' 2a ')
+    #    print SPOTS
+    for x,y in SPOTS:
+        print "num:", countBoard(), "x,y:", x,y, "levels:", LEVEL, LEVEL+1
+        BOARDS[LEVEL+1][x][y] = BOARDS[LEVEL][x][y]
+    #if LEVEL==1:
+    #    printBoard(2, ' 2b ')
+    x, y, xo, yo, xm, ym = move
+    BOARDS[LEVEL+1][x][y] = False
+    BOARDS[LEVEL+1][xm][ym] = False
+    BOARDS[LEVEL+1][xo][yo] = True
+    #if LEVEL==1:
+    #    printBoard(2, ' 2c ')
+
+def countBoard(level=False):
+    sum = 0
+    if level is False:
+        level = LEVEL
+    for row in BOARDS[level]:
+        #print row
+        for col in row:
+            if col == True:
+                sum += 1
+    return sum
+
+def genNext():
+    global LEVEL, MOVES
+    #printBoard()
+    numLeft = countBoard()
+    #print LEVEL, numLeft, " - ", LEVEL + numLeft, " : ", len(listMoves()),
+    #if ((numLeft <= LIMIT and LAST is False) or
+    #    (numLeft <= LIMIT and BOARDS[LEVEL][LAST[0]][LAST[1]] == True)):
+    if (numLeft <= LIMIT):
+        print LEVEL, numLeft, " - ", LEVEL + numLeft
+        print MOVES
+        #for row in board:
+        #    print row
+        print "Level was:", LEVEL
+        testMoves()
+        sys.exit()
+    for move in listMoves():
+        print move,
+        MOVES.append(move)
+        applyMove(move)
+        LEVEL += 1
+        genNext()
+        LEVEL -= 1
+        MOVES.pop()
+
+def printBoard(level=False, prefix=False):
+    if prefix:
+        print ''
+    if level is False:
+        level = LEVEL
+    if not prefix:
+        prefix = ' ' + str(level) + '  '
+    for num, row in enumerate(BOARDS[level]):
+        print prefix,
+        for item in row:
+            if item == True:
+                print 1,
+            elif item == False:
+                print 0,
+            else:
+                print ' ',
+        if num == 0:
+            print countBoard(level)
+        else:
+            print ''
+
+def testMoves():
+    for level in range(LEVEL+1):
+        print ''
+        printBoard(level)
+
+    
+if __name__ == '__main__':
+    #print countBoard(BOARD, 1)
+    LIMIT=1
+    LEVEL=0
+    LAST=False
+    #printBoard()
+    print ''
+    applyMove(MOVES[0])
+    LEVEL=5
+    genNext()
