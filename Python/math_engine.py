77c061429cd2e3aa065ff2157e57756acfbbabee
Dan NOrris <norris.daniel@gmail.com>
2025-12-31 22:01:48 -0800

    kinda got addition working

--- Python/math_engine.py
+++ Python/math_engine.py
@@ -20,6 +20,11 @@ def tokenize(expression):
     We'll have expectains of clean inupt for now, increase
     flixibility later.'''
     tokens = expression.split()
+    for n, token in enumerate(tokens):
+        try:
+            tokens[n] = float(token)
+        except ValueError:
+            pass
     return tokens
 
 
@@ -34,10 +39,10 @@ def structure(tokens, foo=0):
         for n, token in enumerate(tokens):
             if isinstance(token, (list, tuple)):
                 continue
-            if not open_paren and token.endswith('('):
+            if not open_paren and isinstance(token, str) and token.endswith('('):
                 open_paren = n
                 continue
-            if token.endswith('('):
+            if isinstance(token, str) and token.endswith('('):
                 depth += 1
                 continue
             if token == ')' and depth > 0:
@@ -73,6 +78,42 @@ def structure(tokens, foo=0):
     return structured
 
 
+def recurse_apply(structured, func):
+    '''Not sure yet how this shuold work. I'm pretty
+    sure lots of stuff will need to be tested across and 
+    possibly applied to every node/level of the structure.'''
+    if not isinstance(structured, list):
+        return structured
+
+    for i, item in enumerate(structured):
+        if not isinstance(item, list):
+            continue
+        for n, thing in enumerate(item):
+            if n == 0:
+                continue
+            if isinstance(thing, list):
+                recurse_apply(thing, func)
+        structured[i] = func(item)
+    return structured
+
+
+def add_and_subtract(node):
+    '''Evaluate addition and subtraction nodes.'''
+    oper = node[0]
+    if oper not in ('+', '-'):
+        return node
+    if not len(node) == 3:
+        return node
+    for item in node[1:]:
+        if isinstance(item, list):
+            return node
+    if oper == '+':
+        return node[1] + node[2]
+    elif oper == '-':
+        return node[1] - node[2]
+    return node
+
+
 if __name__ == '__main__':
     EXAMPLES = [
         '1 + 2',
@@ -96,5 +137,8 @@ if __name__ == '__main__':
         print(f'Tokens: {TOKENS}')
         STRUCTURED = structure(TOKENS)
         print(f'Structured: {STRUCTURED}')
+        print('Applying addition/subtraction:')
+        STRUCTURED = recurse_apply(STRUCTURED, add_and_subtract)
+        print(f'After addition/subtraction: {STRUCTURED}')
         # result = evaluate(STRUCTURED)
         # print(f'Result: {result}')
